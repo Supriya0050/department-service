d0ef014afa245bfb30e66d70dba906ff0fcbd61b
Supriya More <supriya0050@gmail.com>
2023-04-26 15:39:05 +0200

    Creating using spring boot Department-Service(Eureka Client 1)

---
+++ src/main/java/com/example/departmentservice/entity/Department.java
@@ -0,0 +1,70 @@
+package com.example.departmentservice.entity;
+
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+
+public class Department {
+    
+    private Long id;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+    
+    
+    public Department() {} 
+    
+	public Department(Long id, String departmentName, String departmentAddress, String departmentCode) {
+		super();
+		this.id = id;
+		this.departmentName = departmentName;
+		this.departmentAddress = departmentAddress;
+		this.departmentCode = departmentCode;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "department_name", nullable = false)
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+	@Column(name = "department_address", nullable = false)
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+	
+	@Column(name = "department_code", nullable = false)
+	public String getDepartmentCode() {
+		return departmentCode;
+	}
+	public void setDepartmentCode(String departmentCode) {
+		this.departmentCode = departmentCode;
+	}
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", departmentName=" + departmentName + ", departmentAddress="
+				+ departmentAddress + ", departmentCode=" + departmentCode + "]";
+	}
+    
+	
+	
+    
+}
